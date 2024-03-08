c9cebb9fb9d6272490cf6a05ebc85eb2b866b7f1
hxgabriel <124881877+hxgabriel@users.noreply.github.com>
2024-03-08 09:35:34 -0300

    Add files via upload

---
+++ gha/php/base.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<link rel="html/index.html" href="html/index.html">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-widht, initia-sacale=1.0">
+    <title>variavel</title>
+</head>
+<body>
+     <?php
+     $nome = "Eduardo";
+     const SOBRENOME = " Grando";
+     echo "meu nome eh $nome" . SOBRENOME . "\u{1F9F8} <br>";
+     $peso = 68;
+     $idade = 17;
+     $altura = 1.87;
+     echo "meu peso eh $peso", "<br>";
+     echo "minha altura eh $altura", "<br>";
+     echo "minha idade eh $idade", "<br>";
+     ?>
+</body>
+   </html> 
---
+++ gha/php/hip.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<link rel="html/index.html" href="html/index.html">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-widht, initia-sacale=1.0">
+    <title>Hipotenusa</title>
+</head>
+<body>
+     <?php
+     $hipt = $_GET['hip']['cat1']['cat2'];
+     hip (float $x, float $y): float
+     ?>
+</body>
+   </html> 
+
