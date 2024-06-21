19bd7018e483583d136774b8e24e309044e8f66c
hxgabriel <124881877+hxgabriel@users.noreply.github.com>
2024-06-21 09:34:35 -0300

    Add files via upload

---
+++ gabrielh/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="form.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="mt-5">
+    <h1>Teste</h1>
+  </div>
+
+  <div class="form_cd">
+    <form method="POST">
+      <div><input type="text" name="nome" placeholder="Nome"></div>
+      <div><input type="email" name="email" placeholder="E-mail"></div>
+      <div><input type="number" name="cpf" placeholder="CPF"></div>
+      <div><input type="number" name="telefone" placeholder="Telefone"></div>
+      <div><input type="number" name="autos" placeholder="N° dos Autos"></div>
+      <div><input type="password" name="senha" placeholder="Senha"></div>
+      <div><input type="submit" name="acao" placeholder="Cadastrar"></div>
+      <div><input type="hidden" name="form" placeholder="f_form"></div>
+    </form>
+  </div> <!--form_cd-->
+
+</body>
+
+</html>
